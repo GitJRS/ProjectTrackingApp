2d7005eccf4c9d8d30f271ee4c95d779756e2239
gitspammed <xxxspammalotxxx@gmail.com>
2024-04-13 21:18:50 -0400

    - add cross image asset - configure colors and fonts

---
+++ db-project-tracker/Helpers/FontExtension.swift
@@ -0,0 +1,20 @@
+//
+//  FontExtension.swift
+//  db-project-tracker
+//
+//  Created by MacBook2014 on 4/13/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+  
+  static var screenHeading = Font.system(size: 36, weight: .bold)
+  static var bigHeadline = Font.system(size: 15, weight: .bold)
+  static var smallHeadline = Font.system(size: 10, weight: .bold)
+  static var featuredNumber = Font.system(size: 18)
+  static var featuredText = Font.system(size: 12)
+  static var regularText = Font.system(size: 10)
+  static var captionText = Font.system(size: 8)
+}
